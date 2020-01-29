74bb56da911c954d04217e93548b1a5238af27c0
Sushil Kadu <sushil.kadu@orangebitsindia.com>
2020-01-29 15:23:05 +0530

    Updated code

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>I was changed!</p>
+        <p>I shall contiue to work even harder</p>
         <a
           className="App-link"
           href="https://reactjs.org"
